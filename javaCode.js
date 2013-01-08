7f41c26f90d5eb5c48c7588b35dd18545d76a6c5
cainmd <cainmd@uab.edu>
2013-01-08 08:53:20 -0600

    update java

--- javaCode.js
+++ javaCode.js
@@ -22,7 +22,7 @@ var form1 = document.getElementById("form1");
 
 //document.addEventListener("DOMContentLoaded", whenLoaded(), false);
 var submit = document.getElementById("submit");
-submit.onclick = alert(dataTable.meanGA[1]);
+
 
 //var flinput = document.getElementById("flInput");
 //var crinput = document.getElementById("crInput");
@@ -44,13 +44,13 @@ var AW = document.getElementById("AW");
 
 
 
-var dataTable = {
-meanGA: [12,   9,    7.4,    9.8,	7.1,	N,	N,	29.6,	4.8,	4.8,	4.8,	1.5,	1.4,	1.3,	0.6,	0.9,	0.9,	0.1,	0.1,	0.1,	0.03,	N,	N,	0.01,	N,	N,	0.25,	0.19,	0.19,	0.04,	0.11,	0.11],
+//var dataTable = {
+//meanGA: [12,   9,    7.4,    9.8,	7.1,	N,	N,	29.6,	4.8,	4.8,	4.8,	1.5,	1.4,	1.3,	0.6,	0.9,	0.9,	0.1,	0.1,	0.1,	0.03,	N,	N,	0.01,	N,	N,	0.25,	0.19,	0.19,	0.04,	0.11,	0.11],
 
-stdGA: [3,	1.1,	1.7,	1.1,	N,	N,	14.9,	1.4,	1.4,	1.4,	1.2,	1.2,	1.2,	0.9,	0.9,	0.9,	0.14,	0.14,	0.14,	0.06,	N,	N,	0.02,	N,	N,	0.15,	0.15,	0.15,	0.18,	0.18,	0.18]
-}
+//stdGA: [3,	1.1,	1.7,	1.1,	N,	N,	14.9,	1.4,	1.4,	1.4,	1.2,	1.2,	1.2,	0.9,	0.9,	0.9,	0.14,	0.14,	0.14,	0.06,	N,	N,	0.02,	N,	N,	0.15,	0.15,	0.15,	0.18,	0.18,	0.18]
+//}
 
 
-
+submit.onclick = alert("Here");
 
 
